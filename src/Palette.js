1670b05ea447d816d68c53b6f249d9a844d0b8e2
Edgar Garcia$ git config --global user.name Edgar <edgarciavfx@gmail.com>
2021-07-02 07:47:09 -0500

    add Palette

---
+++ src/Palette.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div>
+        {/* Navbar goes here */}
+        <div className="Palette-colors">
+          {/* bunch of color boxes */}
+        </div>
+      </div>
+    )
+  }
+}
