138db0c28ba61a42680c47eabcb6b41d96089c1d
Edgar Garcia$ git config --global user.name Edgar <edgarciavfx@gmail.com>
2021-07-05 08:58:04 -0500

    add Navbar

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
 import './Palette.css';
+import Navbar from './Navbar';
 
 export default class Palette extends Component {
   constructor(props) {
@@ -21,15 +20,7 @@ export default class Palette extends Component {
     ));
     return (
       <div className="Palette">
-        <div className="slider">
-          <Slider
-            defaultValue={level}
-            min={100}
-            max={900}
-            step={100}
-            onAfterChange={this.changeLevel}
-          />
-        </div>
+        <Navbar level={level} changeLevel={this.changeLevel} />
         {/* Navbar goes here */}
         <div className="Palette-colors">{colorBoxes}</div>
         {/* footer eventually */}
