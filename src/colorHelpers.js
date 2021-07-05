e64285cc1c71b68ec53f4c3c56a3dfeef928cddc
Edgar Garcia$ git config --global user.name Edgar <edgarciavfx@gmail.com>
2021-07-05 08:39:51 -0500

    style colorSlider

--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -19,7 +19,7 @@ function generatePalette(starterPalette) {
         id: color.name.toLowerCase().replace(/ /g, '-'),
         hex: scale[i],
         rgb: chroma(scale[i]).css(),
-        rgba: chroma(scale[i]) 
+        rgba: chroma(scale[i])
           .css()
           .replace('rgba', 'rgba')
           .replace(')', ',1.0)'),
