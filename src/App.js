dbd6286e5424849916f373ef7baaaf54e3ff2429
Edgar Garcia$ git config --global user.name Edgar <edgarciavfx@gmail.com>
2021-07-06 08:24:11 -0500

    add react router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import Palette from './Palette';
 import seedPalettes from './seedPalettes';
 import { generatePalette } from './colorHelpers';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Palette palette={generatePalette(seedPalettes[4])} />
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
+        <Route exact path='/palette/:id' render={() => <h1>Individual Palette</h1>} />
+      </Switch>
+      // <div>
+      //   <Palette palette={generatePalette(seedPalettes[4])} />
+      // </div>
     );
   }
 }
