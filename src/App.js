72471c3ef6081ec189d51a1922851258a0f1e1c6
Edgar Garcia$ git config --global user.name Edgar <edgarciavfx@gmail.com>
2021-07-06 08:49:40 -0500

    add PaletteList and Links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Palette from './Palette';
 import seedPalettes from './seedPalettes';
 import { generatePalette } from './colorHelpers';
 import { Route, Switch } from 'react-router-dom';
+import PaletteList from './PaletteList';
 
 class App extends Component {
   findPalette(id) {
@@ -11,7 +12,11 @@ class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
+        <Route
+          exact
+          path="/"
+          render={() => <PaletteList palettes={seedPalettes} />}
+        />
         <Route
           exact
           path="/palette/:id"
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
@@ -29,7 +30,7 @@ export default class Navbar extends Component {
     return (
       <header className="Navbar">
         <div className="logo">
-          <a href="#">reactColorPicker</a>
+          <Link to="/">reactColorPicker</Link>
         </div>
         <div className="slider-container">
           <span>Level: {level}</span>
