813d482065845c0f18ae0c01487da9094b99fe19
Edgar Garcia$ git config --global user.name Edgar <edgarciavfx@gmail.com>
2021-07-06 13:09:08 -0500

    add singleColorPalette component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import seedPalettes from './seedPalettes';
 import { generatePalette } from './colorHelpers';
 import { Route, Switch } from 'react-router-dom';
 import PaletteList from './PaletteList';
+import SingleColorPalette from './SingleColorPalette';
 
 class App extends Component {
   findPalette(id) {
@@ -33,7 +34,7 @@ class App extends Component {
         <Route
           exact
           path="/palette/:paletteId/:colorId"
-          render={() => <h1>Shades of color</h1>}
+          render={() => <SingleColorPalette/>}
         />
       </Switch>
       // <div>
